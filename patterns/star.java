7509707ec9450fac30b3db4ebabe9a54d4c40f2b
A R Ashutosh <arashutosh01@gmail.com>
2023-11-07 01:34:03 +0530

    complete pattern (part 1)

--- patterns/star.java
+++ patterns/star.java
@@ -1,20 +1,48 @@
-/*
- * 
- * *
- * * *
- * * * *
- */
-
 
 package patterns;
 public class star{
     public static void main(String args[]){
-        for(int i=0; i<5; i++){
+        //Star(10);
+        //InvertedStar(10);
+        //HalfPyramid(10);
+        characterPattern(4);
+    }
+    public static void Star(int n){
+        for(int i=0; i<n; i++){
             for(int j=0; j<i; j++){
                 System.out.print("*");
             }
             System.out.println();
         }
     }
-}
 
+    public static void InvertedStar(int n){
+        for(int i=n; i>0; i--){
+            for(int j=0; j<i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void HalfPyramid(int n){
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void characterPattern(int n){
+        char ch = 'A';
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print(ch);
+                ch++;
+                
+            }
+            System.out.println();
+        }
+    }
+}
